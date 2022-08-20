cb57107b336b2738967c87b2b420665871a39b4f
Ghaldszar <ghaldszar@gmail.com>
2022-08-20 09:52:06 +0300

    Add files via upload

---
+++ HipotenusveAlanHesapla.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class HipotenusveAlanHesapla {
+
+    public static void main(String[] args) {
+    	
+    	int a , b , alan;
+    	double hipotenus;
+
+    	Scanner input = new Scanner(System.in);
+
+    	System.out.print("1.Kenarın Uzunluğunu Giriniz : ");
+    	a = input.nextInt();
+
+    	System.out.print("1.Kenarın Uzunluğunu Giriniz : ");
+    	b = input.nextInt();
+
+    	hipotenus = Math.sqrt(Math.pow(a , 2) + Math.pow(b , 2));
+
+    	System.out.print("Hipotenüs Uzunluğu : " + hipotenus);
+    	System.out.println();
+
+    	alan = a * b;
+    	System.out.print("Üçgenin Alanı : " + alan);
+
+    }
+}
+
+

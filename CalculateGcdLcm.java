682553dc77219562f19a9e144e725a6fe9f2dbea
Ghaldszar <ghaldszar@gmail.com>
2022-12-05 23:36:08 +0300

    Create CalculateGcdLcm.java
    Verilen 2 değerin OBEB ve OKEK hesaplayan Java programı

---
+++ CalculateGcdLcm.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class CalculateGcdLcm {
+
+    public static void main(String[] args) {
+    	
+    	obeb();
+    	okek();
+
+    }
+
+    static void obeb() {
+    	
+    	int bigNumber = 0;
+    	int smallNumber = 0;
+    	int commonMultiply = 1;
+
+    	Scanner keybInput = new Scanner(System.in);
+
+    	System.out.print("İlk Sayı : ");
+    	int firstNumber = keybInput.nextInt();
+    	System.out.print("İkinci Sayı : ");
+    	int secondNumber = keybInput.nextInt();
+
+    	if (firstNumber > secondNumber) {
+    		bigNumber = firstNumber;
+    		smallNumber = secondNumber;
+    	}
+    	else if (firstNumber < secondNumber) {
+    		smallNumber = firstNumber;
+    		bigNumber = secondNumber;
+    	}
+
+    	for (int i = 2 ; i <= smallNumber ; i++) {
+    		while (smallNumber % i == 0 && bigNumber % i == 0){
+    			smallNumber /= i;
+    			bigNumber /= i;
+    			commonMultiply *= i;
+    		}
+    	}
+    	System.out.printf("OBEB : %d \n" , commonMultiply);
+    }
+
+    static void okek() {
+
+    	int bigNumber = 0;
+    	int smallNumber = 0;
+    	int commonMultiply = 1;	
+
+    	Scanner keybInput = new Scanner(System.in);
+
+    	System.out.print("İlk Sayı : ");
+    	int firstNumber = keybInput.nextInt();
+    	System.out.print("İkinci Sayı : ");
+    	int secondNumber = keybInput.nextInt();
+
+    	if (firstNumber > secondNumber) {
+    		bigNumber = firstNumber;
+    		smallNumber = secondNumber;
+    	}
+    	else if (firstNumber < secondNumber) {
+    		smallNumber = firstNumber;
+    		bigNumber = secondNumber;
+    	}
+
+    	for (int i = 2 ; i <= bigNumber ; i++) {
+    		while (bigNumber % i == 0 && smallNumber % i == 0){
+    			bigNumber /= i;
+    			smallNumber /= i;
+    			commonMultiply *= i;
+    		}
+    		while (bigNumber % i == 0){
+    			bigNumber /= i;
+    			commonMultiply *= i;   			
+    		}
+    		while (smallNumber % i ==	 0){
+    			smallNumber /= i;
+    			commonMultiply *= i;
+    		}
+    	}
+    	System.out.printf("OKEK : %d \n" , commonMultiply);
+
+    }
+}
+
+
